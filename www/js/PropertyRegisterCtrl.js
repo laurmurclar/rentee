e3b9bd8beed782130e95a52443937c134f148672
cihegart <cihegart@tcd.ie>
2016-03-30 10:49:22 +0100

    search works - buttons dont show ticks correctly, property register doesn't work

--- www/js/PropertyRegisterCtrl.js
+++ www/js/PropertyRegisterCtrl.js
@@ -1,26 +1,25 @@
 angular.module('rentee.controllers')
   .controller('PropertyRegisterCtrl', function($scope, $auth) {
     $scope.handleRegBtnClick = function(form) {
-      $auth.submitRegistration(
-      {
-        street: form.street,
-        town: form.town,
-        county: form.county, 
-        rent:                   form.rent,
-        n_baths:                form.n_baths,
-        retn_allowance:         form.rent_allowance,
-        ptrb:                   form.ptrb,
-        n_beds:                 form.n_beds
-      }, {
-        config: 'property'
-      })
-        .then(function(resp) {
-          // handle success response
-          console.log("works");
-        })
-        .catch(function(resp) {
-          // handle error response
-          console.log("is poo");
-        });
-    };
-  });
+      $http({
+        url: "https://rentee-api.herokuapp.com/property", 
+        method: "POST",
+        params: 
+        {
+          street:                 form.street,
+          town:                   form.town,
+          county:                 form.county, 
+          rent:                   form.rent,
+          n_beds:                 form.n_beds,
+          n_baths:                form.n_baths,
+          rent_allowance:         form.rent_allowance,
+          ptrb:                   form.ptrb
+      }
+    }).then( function successCallback(response){
+        // do something with the reponse containing the landlord details
+        console.log("property register works");
+      }, function errorCallback(response){
+        console.log("property register no work");
+      });
+  }
+    });
--- www/js/SearchSettingsCtrl.js
+++ www/js/SearchSettingsCtrl.js
@@ -1,28 +1,36 @@
 angular.module('rentee.controllers')
-  .controller('SearchSettingsCtrl', function($scope, $auth) {
-
-    $scope.handleRegBtnClick = function(form) {
-      //$scope.rent_allowance = 'false'; //to make default value of rent allowance false - needs to be tested
-      //$scope.ptrb = 'false'; //to make default value of ptrb false - needs to be tested
-      $auth.submitRegistration(
+  .controller('SearchSettingsCtrl', function($scope, $stateParams, $http) {
+      $scope.handleRegBtnClick = function(form){
+      //form.rent_allowance = 'f'; 
+      //form.ptrb = 'f'; 
+      /*if(form.rent_allowance=='t')
       {
-        town:                form.town,
-        county:                form.county, 
-        rent:                   form.rent,
-        n_beds:                 form.n_beds,
-        n_baths:                form.n_baths,
-        rent_allowance:         form.rent_allowance,
-        ptrb:                   form.ptrb
-      }, {
-        config: 'search'
-      })
-        .then(function(resp) {
-          // handle success response
-          console.log("work wok wok");
-        })
-        .catch(function(resp) {
-          // handle error response
-          console.log("no working");
-        });
-    };
-  });
+        form.rent_allowance='t';
+      }
+      if(form.ptrb='t')//??? wtf does this work?
+      {
+        form.ptrb='t';
+      }*/
+      $scope.tenant = JSON.parse(window.localStorage['user'] || '{}');
+      $http({
+        url: "https://rentee-api.herokuapp.com/property", 
+        method: "GET",
+        params: 
+        {
+          town:                form.town,
+          county:                form.county, 
+          rent:                   form.rent,
+          n_beds:                 form.n_beds,
+          n_baths:                form.n_baths,
+          rent_allowance:         form.rent_allowance,
+          ptrb:                   form.ptrb,
+          t_id:                   $scope.tenant.id
+        }
+    }).then( function successCallback(response){
+        // do something with the reponse containing the landlord details
+        console.log("works");
+      }, function errorCallback(response){
+        console.log("Error in request for landlord details");
+      });
+  }
+    });
