d478e27a5793621450a3bd34b7052d71d5427d0b
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-03-31 19:16:03 +0100

    small change to how property page updates after new search
    ensures that true/false get changed to yes/no. also, no need to change
    state

--- www/js/PropertyProfileCtrl.js
+++ www/js/PropertyProfileCtrl.js
@@ -1,13 +1,18 @@
 angular.module('rentee.controllers')
   .controller('PropertyProfileCtrl', function($scope, $auth, $http, $state) {
+
+    var updatePropertyPage = function () {
+      $scope.search = JSON.parse(window.localStorage['searchResult']);
+
+      if ($scope.search.rent_allowance == true) $scope.search.rent_allowance = 'yes';
+      else if ($scope.search.rent_allowance == false) $scope.search.rent_allowance = 'no';
+
+      if ($scope.search.ptrb == true) $scope.search.ptrb = 'yes';
+      else if ($scope.search.ptrb == false) $scope.search.ptrb = 'no';
+    };
+
     // Setup the page with the most recent search result
-    $scope.search = JSON.parse(window.localStorage['searchResult']);
-
-    if ($scope.search.rent_allowance == true) $scope.search.rent_allowance = 'yes';
-    else if ($scope.search.rent_allowance == false) $scope.search.rent_allowance = 'no';
-
-    if ($scope.search.ptrb == true) $scope.search.ptrb = 'yes';
-    else if ($scope.search.ptrb == false) $scope.search.ptrb = 'no';
+    updatePropertyPage();
 
     var user = JSON.parse(window.localStorage['user']);
 
@@ -27,41 +32,40 @@ angular.module('rentee.controllers')
       });
     };
 
-      var repeatSearch = function() {
-        var form = JSON.parse(window.localStorage['searchForm']);
-        var tenant = JSON.parse(window.localStorage['user']);
+    var repeatSearch = function() {
+      var form = JSON.parse(window.localStorage['searchForm']);
+      var tenant = JSON.parse(window.localStorage['user']);
 
-        $http({
-          url: "https://rentee-api.herokuapp.com/property",
-          method: "GET",
-          params:
-          {
-            town:                   form.town,
-            county:                 form.county,
-            rent:                   form.rent,
-            n_beds:                 form.n_beds,
-            n_baths:                form.n_baths,
-            rent_allowance:         form.rent_allowance,
-            ptrb:                   form.ptrb,
-            t_id:                   tenant.id
+      $http({
+        url: "https://rentee-api.herokuapp.com/property",
+        method: "GET",
+        params:
+        {
+          town:                   form.town,
+          county:                 form.county,
+          rent:                   form.rent,
+          n_beds:                 form.n_beds,
+          n_baths:                form.n_baths,
+          rent_allowance:         form.rent_allowance,
+          ptrb:                   form.ptrb,
+          t_id:                   tenant.id
+        }
+      }).then( function successCallback(response){
+          var lastSearchResult = JSON.stringify(response.data);
+
+          if (lastSearchResult == 'null'){
+            // There was no properties which matched the criteria
+            $state.go('no-property-results');
           }
-        }).then( function successCallback(response){
-            var lastSearchResult = JSON.stringify(response.data);
+          else {
+            // Save the most recent search criteria (for reuse) and the result (for displaying on next screen)
+            window.localStorage['searchResult'] = lastSearchResult;
+            window.localStorage['searchForm'] = JSON.stringify(form);
 
-            if (lastSearchResult == 'null'){
-              // There was no properties which matched the criteria
-              $state.go('no-property-results');
-            }
-            else {
-              // Save the most recent search criteria (for reuse) and the result (for displaying on next screen)
-              window.localStorage['searchResult'] = lastSearchResult;
-              window.localStorage['searchForm'] = JSON.stringify(form);
-
-              $scope.search = JSON.parse(window.localStorage['searchResult']);
-              $state.go('property-profile');
-            }
-        }, function errorCallback(response){
-          console.log("Error in search request");
-        });
+            updatePropertyPage();
+          }
+      }, function errorCallback(response){
+        console.log("Error in search request");
+      });
     };
   });
